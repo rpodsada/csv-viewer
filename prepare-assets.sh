d66bdd07f483228acf8daf1769932e12b005f7ce
Richard Podsada <rpodsada@gmail.com>
2023-06-11 21:14:52 -0600

    Ensure build directory, fix paths

--- prepare-assets.sh
+++ prepare-assets.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Ensure directory exists
+if [ ! -d "./build/" ]; then
+    mkdir build/
+fi
+
 # Copy index.html
 cp src/index.html build/
 
 # Copy css/
-cp -r src/css/ build/css/
+cp -a src/css/ build/
 
 # Copy fonts/
-cp -r src/fonts/ build/fonts/
+cp -a src/fonts/ build/
